2f568ac6725843889398901637da5cf6c128501b
Tyler Mayoff <tyler@tylermayoff.com>
2022-09-05 14:18:51 -0400

    typo

--- climate.py
+++ climate.py
@@ -15,7 +15,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 from homeassistant.helpers.update_coordinator import (
-    CoordinatorEntitity,
+    CoordinatorEntity,
     DataUpdateCoordinator,
     UpdateFailed,
 )
