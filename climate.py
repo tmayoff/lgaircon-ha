14fe79306747d7e71f451bc7cdcb4d20e307c09d
Tyler Mayoff <tyler@tylermayoff.com>
2022-09-05 15:13:14 -0400

    removed logging

--- climate.py
+++ climate.py
@@ -94,8 +94,6 @@ class LGAircon(CoordinatorEntity, ClimateEntity):
 
     @callback
     def _handle_coordinator_update(self) -> None:
-        _LOGGER.info("Updating")
         if self.coordinator.data:
-            _LOGGER.info(f"Data {coordinator.data}")
             self._current_temp = self.coordinator.data["cur_temp"]
             self.async_write_ha_state()
