af22ca09aa011961fc3164329ada84839689e8bd
Tyler Mayoff <tyler@tylermayoff.com>
2022-09-05 14:11:05 -0400

    .

--- climate.py
+++ climate.py
@@ -30,6 +30,12 @@ class LGAirconCoordinator(DataUpdateCoordinator):
     def __init__(self, hass, api):
         self.api = api
 
+    async def _async_update_data(self):
+        """Fetch data from API endpont"""
+
+        try:
+            
+
 class LGAircon(ClimateEntity):
     _attr_has_entity_name = True
 
