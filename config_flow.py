7f6bb716238e215bb0f63e809d116967ed5c3665
Tyler Mayoff <tyler@tylermayoff.com>
2022-09-12 21:17:08 -0400

    .

--- config_flow.py
+++ config_flow.py
@@ -3,9 +3,9 @@ from homeassistant import config_entries
 DOMAIN = "lgaircon"
 
 class LGAirconConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    # async def async_step_dhcp(self, info):
+    async def async_step_dhcp(self, info):
     #     await self.async_set_unique_id(device_unique_id)
     #     self._abort_if_unique_id_configured()
-    return self.async_show_form(
-        step_id="dhcp"
-    )
+        return self.async_show_form(
+            step_id="dhcp"
+        )
